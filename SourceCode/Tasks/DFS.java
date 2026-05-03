811d2c781da0a9fe15a81ce803038a94fe94e6a1
Mark Gonzalez <Markg3@cpp.edu>
2026-05-03 14:43:30 -0700

    initial commit, created a list

--- SourceCode/Tasks/DFS.java
+++ SourceCode/Tasks/DFS.java
@@ -1,1 +1,7 @@
 //depth-first tree
+/**
+ * make a tree and traverse it in depth-first order
+ * create a stack and visited set to keep track of visited nodes.
+ * loop while the stack is not empty.
+ * pop the current node and print it
+ */
