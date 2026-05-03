ec64f319e17962cb5cc5665a40faabf3339fe98c
Mark Gonzalez <Markg3@cpp.edu>
2026-05-03 14:55:54 -0700

    dfs added

--- SourceCode/Tasks/DFS.java
+++ SourceCode/Tasks/DFS.java
@@ -5,3 +5,32 @@
  * loop while the stack is not empty.
  * pop the current node and print it
  */
+import java.util.*;
+public class DFS {
+    private Graph graph;
+
+    public DFS(Graph graph) {
+        this.graph = graph;
+    }
+
+    public void traverse(String start){
+        LinkedStack<String> stack = new LinkedStack<>();
+        Set<String> visited = new HashSet<>();
+
+        stack.push(start);
+        visited.add(start);
+        System.out.print("DFS Order: ");
+
+        while (!stack.isEmpty()) {
+            String current = stack.pop();
+            System.out.print(current + " ");
+
+            for (String neighbor : graph.getNeighbors(current)) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+}
